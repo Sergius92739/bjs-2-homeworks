4d85b5918fceaf10f66e2152efb639d58b8ee2d0
Sergius92739 <sergem279@gmail.com>
2023-04-26 23:41:16 +0300

    initial commit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,55 @@
-﻿
+﻿// Задача 1
+function parseCount (value) {
+  const result = Number.parseFloat(value);
+
+  if (isNaN(result)) {
+    throw new Error('Невалидное значение');
+  }
+
+  return result;
+}
+
+function validateCount (value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
+
+// Задача 2
+class Triangle {
+  constructor(a, b, c) {
+    if (a + b < c || b + c < a || a + c < b) {
+      throw new Error('Треугольник с такими сторонами не существует')
+    }
+
+    this.a = a;
+    this.b = b;
+    this.c = c;
+  }
+
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  get area() {
+    const halfMeter = this.perimeter / 2;
+    return +Math.sqrt(halfMeter * (halfMeter - this.a) * (halfMeter - this.b) * (halfMeter - this.c)).toFixed(3);
+  }
+}
+
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch (error) {
+    return {
+      get perimeter() {
+        return 'Ошибка! Треугольник не существует';
+      },
+      get area() {
+        return 'Ошибка! Треугольник не существует';
+      }
+    }
+  }
+}
